f284fb9f0df8a61fd8f299cc1cb69f9c9de52127
KetraKrato <60010304@kmitl.ac.th>
2020-10-14 23:59:36 +0700

    CustomLoginPage

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,22 @@ import { Navigate } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/DashboardLayout';
 import MainLayout from 'src/layouts/MainLayout';
 import AccountView from 'src/views/account/AccountView';
-import CustomerListView from 'src/views/customer/CustomerListView';
+// import CustomerListView from 'src/views/customer/CustomerListView';
 import DashboardView from 'src/views/reports/DashboardView';
 import LoginView from 'src/views/auth/LoginView';
 import NotFoundView from 'src/views/errors/NotFoundView';
 import ProductListView from 'src/views/product/ProductListView';
 import RegisterView from 'src/views/auth/RegisterView';
 import SettingsView from 'src/views/settings/SettingsView';
+import LoginPage from 'src/views/Login/LoginPage';
+import { element } from 'prop-types';
+
+import LoginLayout from 'src/layouts/LoginLayout';
 
 const routes = [
   {
 
-    // add route  dashboard login and  video  page 
+    // add route  dashboard login and  video  page
     path: 'app',
     element: <DashboardLayout />,
     children: [
@@ -34,9 +38,17 @@ const routes = [
       { path: 'register', element: <RegisterView /> },
       { path: '404', element: <NotFoundView /> },
       { path: '/', element: <Navigate to="/app/dashboard" /> },
-      { path: '*', element: <Navigate to="/404" /> }
+      { path: '*', element: <Navigate to="/404" /> },
+      // { path: 'loginpage', element: <LoginPage /> },
     ]
-  }
+  },
+  {
+    path: '/',
+    element: <LoginLayout />,
+    children: [
+      { path: 'loginpage', element: <LoginPage /> },
+    ]
+  },
 ];
 
 export default routes;
--- src/views/Login/LoginPage.js
+++ src/views/Login/LoginPage.js
@@ -4,34 +4,103 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 import {
   Box,
-  Button,
+  // Buon,
   Container,
-  Grid,
-  Link,
+  // Grid,
+  // Link,
   TextField,
   Typography,
-  makeStyles
+  makeStyles,
 } from '@material-ui/core';
-import FacebookIcon from 'src/icons/Facebook';
-import GoogleIcon from 'src/icons/Google';
+// import FacebookIcon from 'src/icons/Facebook';
+// import GoogleIcon from 'src/icons/Google';
 import Page from 'src/components/Page';
+// import { fromPairs } from 'lodash';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#F0F0F7',
+    height: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  },
+  box: {
+    backgroundColor: '#6865A5',
+    height: 850,
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  },
 
+}));
 const LoginPage = () => {
   const classes = useStyles();
   const navigate = useNavigate();
- return (
-   <Page 
-    className={classes.root}
-    title="Login"
-   >
-     <Box>
-        <Container>
-          
-        </Container>
-     </Box>
 
-   </Page>
- );
+  return (
+    <Page
+      className={classes.root}
+      title="Login"
+    >
+      <Box display="flex" flexDirection="colum" height="100%" justifyContent="center ">
+        <Container maxWidth="sm" color="black">
+          <Formik
+            initialValues={{ email: 'test@gmail.com', password: '1234567' }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+              password: Yup.string().max(255).required('Password is required')
+            })}
+           // onSubmit={() => { navigate('/app/dashboard', { replace: true }); }}
+          >
+            {({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <Box>
+                  <Typography
+                    align="center"
+                    color="textSecondary"
+                    variant="body1"
+                  >
+                    or login with email address
+                  </Typography>
+                </Box>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Password"
+                  name="password"
+                  autoComplete="email"
+                  type="password"
+                  autoFocus
+                />
+                <Typography
+                  align="center"
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  or login with email address
+                </Typography>
+              </form>
+
+            )}
+
+          </Formik>
+        </Container>
+      </Box>
+
+    </Page>
+  );
 };
 export default LoginPage;
